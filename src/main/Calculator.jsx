ea5a5655a462f8181ed20e10ccf5539054ff32e0
rmshimomura <shimomurarodrigo1@gmail.com>
2022-01-19 14:24:35 -0300

    Button component added

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Calculator.css'
 
+import Button from '../components/Button';
+
 export default class Calculator extends Component {
 
     render() {
@@ -8,7 +10,23 @@ export default class Calculator extends Component {
         return(
 
             <div className="calculator">
-                
+                <Button label="AC"/>
+                <Button label="/"/>
+                <Button label="7"/>
+                <Button label="8"/>
+                <Button label="9"/>
+                <Button label="*"/>
+                <Button label="4"/>
+                <Button label="5"/>
+                <Button label="6"/>
+                <Button label="-"/>
+                <Button label="1"/>
+                <Button label="2"/>
+                <Button label="3"/>
+                <Button label="+"/>
+                <Button label="0"/>
+                <Button label="."/>
+                <Button label="="/>
             </div>
 
         )
