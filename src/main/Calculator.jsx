a32a17032123b1ebf50bfcbfd4067e899652668f
rmshimomura <shimomurarodrigo1@gmail.com>
2022-01-19 14:31:15 -0300

    Display component created

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Calculator.css'
 
 import Button from '../components/Button';
+import Display from '../components/Display';
 
 export default class Calculator extends Component {
 
@@ -10,6 +11,7 @@ export default class Calculator extends Component {
         return(
 
             <div className="calculator">
+                <Display value={1000}/>
                 <Button label="AC"/>
                 <Button label="/"/>
                 <Button label="7"/>
